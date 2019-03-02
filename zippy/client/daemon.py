88cd937c5ab970a4fe4ac7a96300029588923e49
Saugat Pachhai <suagatchhetri@outlook.com>
2019-03-02 12:42:59 +0545

    Make it true daemon

--- zippy/client/daemon.py
+++ zippy/client/daemon.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 """Background running program that checks for new mails and downloads them."""
+import atexit
 import concurrent.futures
 import os
 import pathlib
@@ -14,6 +15,9 @@ import yaml
 
 from imapclient import IMAPClient
 from imapclient.exceptions import LoginError
+from log import get_logger
+
+LOGGER = get_logger()
 
 
 class Daemon:
@@ -29,18 +33,63 @@ class Daemon:
         if self.disable_daemon:
             return
 
-        pid = os.fork()
+        LOGGER.debug("Daemonizing...")
 
-        if pid > 0:
-            sys.exit(0)
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write("fork #1 failed: {0}\n".format(err))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # do second fork. why?
+        # see: https://stackoverflow.com/q/881388
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write("fork #2 failed: {0}\n".format(err))
+            sys.exit(1)
+
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        stdin = open(os.devnull, "r")
+        stdout = open(os.devnull, "a+")
+        stderr = open(os.devnull, "a+")
+        os.dup2(stdin.fileno(), sys.stdin.fileno())
+        os.dup2(stdout.fileno(), sys.stdout.fileno())
+        os.dup2(stderr.fileno(), sys.stderr.fileno())
+
+        LOGGER.debug("Daemonized successfully.")
+
+        # write pid file
+
+        signal.signal(signal.SIGTERM, self.stop)
+        signal.signal(signal.SIGINT, self.stop)
+
+        atexit.register(self._remove_pidfile)
+        self._save_pid()
+
+    def _remove_pidfile(self):
+        LOGGER.info("Removing PID file...")
+        self.PID_FILE.unlink()
 
     def start(self):
         """Start daemon."""
-        if self.PID_FILE.exists():
-            pid = self._get_pid()
-            print(f"Another instance is already running with pid: {pid}")
+        pid = self._get_pid()
+        if pid:
+            sys.stderr.write(f"Another instance with pid: {pid} already running.\n")
             sys.exit(1)
-
         self._daemonize()
         self._save_pid()
 
@@ -53,14 +102,14 @@ class Daemon:
 
     def __exit__(self, *args):
         """Context manager that destroys daemon."""
-        pid = self.stop()
-        sys.exit(0 if pid else 1)
+        self.stop()
+        sys.exit(0)
 
     def _get_pid(self):
         if self.PID_FILE.exists():
             pid = int(self.PID_FILE.read_text().strip())
             return pid
-        return 0
+        return None
 
     def _save_pid(self):
         with self.PID_FILE.open("w") as pid_file:
@@ -69,13 +118,27 @@ class Daemon:
             pid_file.flush()
 
     def stop(self):
-        """Stop daemon."""
+        """Kill the running daemon."""
+        # Try killing the daemon process
         pid = self._get_pid()
-        if pid:
-            self.PID_FILE.unlink()
-            os.kill(pid, signal.SIGTERM)
-            return pid
-        return 0
+
+        if not pid:
+            sys.stderr.write(
+                f"pid file {self.PID_FILE} does not exist. Daemon not running?\n"
+            )
+            return
+
+        LOGGER.debug("Shutting down the daemon.")
+
+        while True:
+            try:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.2)
+            except OSError:
+                if self.PID_FILE.exists():
+                    self.PID_FILE.unlink()
+                    LOGGER.info("Removed PID file")
+                break
 
 
 def download_new_email(username, password, host, imap_port=993, timeout=3):
@@ -102,51 +165,74 @@ def download_new_email(username, password, host, imap_port=993, timeout=3):
     .. todo:: Downloading emails when notified, as currently is only gets
         notifications.
     """
+    # We are using ssl but let's not enforce it strictly
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    ssl_context.verify_flags = ssl.CERT_NONE
+    ssl_context.verify_flags = ssl.CERT_OPTIONAL
 
-    print(username)
+    LOGGER.debug(username)
 
     try:
         server = IMAPClient(
             host, port=imap_port, ssl=True, ssl_context=ssl_context, timeout=timeout
         )
     except socket.error:
-        print("Could not connect to the mail server.")
-        raise
+        LOGGER.exception("Could not connect to the mail server.")
+        return
     except ssl.SSLError:
-        print("Error due to ssl.")
-        raise
+        LOGGER.exception("Error due to ssl.")
+        return
 
     try:
         server.login(username, password)
+        server.select_folder("INBOX")
     except LoginError:
-        print(f"Credentials could not be verified for '{username}'.")
+        LOGGER.exception("Credentials could not be verified for '%s'.", username)
         server.shutdown()
-        raise
-
-    server.select_folder("INBOX")
+        return
 
     while True:
+        # reconnect every 7 mins, as per recommendation to do so.
         timeout = time.time() + 7 * 60  # after 7 mins
 
         # Start IDLE mode
-        server.idle()
-        print("Connection is now in IDLE mode, send yourself an email or quit with ^c")
+        try:
+            server.idle()
+        except Exception:  # pylint: disable=broad-except
+            LOGGER.exception("Unexpected exception occurred.")
+            return
+        else:
+            LOGGER.info(
+                "Connection is now in IDLE mode, send yourself an email or quit with ^c"
+            )
 
         curr_time = time.time()
         while curr_time < timeout:
-            # Wait for up to 30 seconds for an IDLE response
-            responses = server.idle_check(timeout=30)
-            print(
-                "Server sent:",
-                responses if responses else "nothing",
-                f" for {username}",
-            )
+            try:
+                # Wait for up to 30 seconds for an IDLE response
+                responses = server.idle_check(timeout=30)
+                # if server was up, response should have come as timeout is 30sec.
+                # so, it's not required to check for socket.error here.
+            # log the unexpected exception
+            except Exception:  # pylint: disable=broad-except
+                LOGGER.exception("Unexpected error occurred.")
+                return
+            else:
+                LOGGER.info(
+                    "Server sent: %s for %s",
+                    responses if responses else "nothing",
+                    username,
+                )
+            time.sleep(10)
 
-        server.idle_done()
-        print("\nIDLE mode done")
-        time.sleep(10)
+        try:
+            server.idle_done()
+        # nothing expected exception here. but let's log these if it occurs.
+        except Exception:  # pylint: disable=broad-except
+            LOGGER.exception("Unexpected error occurred.")
+            return
+        else:
+            LOGGER.info("\nIDLE mode done")
+            time.sleep(10)
 
 
 if __name__ == "__main__":
@@ -154,31 +240,31 @@ if __name__ == "__main__":
     CONFIG = yaml.safe_load(STREAM)
     CLIENT_CONFIG = CONFIG.get("CLIENT")
     if not CLIENT_CONFIG:
-        print("Problem with config.")
+        print("Config not setup properly. Check env.example.yml for more info.")
         sys.exit(1)
 
     if len(sys.argv) > 1 and sys.argv[1] == "stop":
-        PID = Daemon().stop()
-        if PID == 0:
-            print("No other instance seems to be running.")
-        sys.exit(0 if PID else 1)
+        Daemon().stop()
     else:
         if len(sys.argv) > 1 and sys.argv[1] == "restart":
             Daemon().stop()
+            MESSAGE = "Daemon restarted."
+            LOGGER.info(MESSAGE)
+            print(MESSAGE)
 
-    CREDENTIALS = CLIENT_CONFIG.get("users")
+        CREDENTIALS = CLIENT_CONFIG.get("users")
 
-    with Daemon(disable=CLIENT_CONFIG.get("debug", False)) as d:
-        with concurrent.futures.ThreadPoolExecutor(
-            max_workers=len(CREDENTIALS)
-        ) as executor:
-            for credential in CREDENTIALS:
-                executor.submit(
-                    download_new_email,
-                    credential["username"],
-                    credential["password"],
-                    CLIENT_CONFIG["hostname"],
-                    CLIENT_CONFIG.get("imap_port", 993),
-                    CLIENT_CONFIG.get("timeout", 3),
-                )
-            executor.shutdown(wait=True)
+        with Daemon(disable=CLIENT_CONFIG.get("debug", False)) as d:
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=len(CREDENTIALS)
+            ) as executor:
+                for credential in CREDENTIALS:
+                    executor.submit(
+                        download_new_email,
+                        credential["username"],
+                        credential["password"],
+                        CLIENT_CONFIG["hostname"],
+                        CLIENT_CONFIG.get("imap_port", 993),
+                        CLIENT_CONFIG.get("timeout", 3),
+                    )
+                executor.shutdown(wait=True)
