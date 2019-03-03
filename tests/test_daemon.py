71a826262fcf8869a15cfb40dda485af08080961
Saugat Pachhai <suagatchhetri@outlook.com>
2019-03-03 08:42:21 +0545

    Add unittest for daemon

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -1,3 +1,4 @@
+# pylint: disable=redefined-outer-name
 """Test for client daemon."""
 import os
 import pathlib
@@ -7,16 +8,49 @@ import time
 
 from subprocess import Popen
 
+import mock
 import pytest
+import yaml
+
+from src.client import daemon
+
+
+@pytest.fixture
+def env_with_config():
+    """Create an environment with ZIPPY_CONFIG appended."""
+    config = {
+        "CLIENT": {
+            "hostname": "localhost.org",
+            "users": [
+                {"username": "test0@localhost.org", "password": "test0"},
+                {"username": "test1@localhost.org", "password": "test1"},
+            ],
+        }
+    }
+
+    config_file = pathlib.Path(tempfile.gettempdir()) / "env_config.yml"
+
+    yaml.safe_dump(config, open(config_file, "w"))
+
+    new_env = os.environ.copy()
+    new_env["ZIPPY_CONFIG_FILE"] = str(config_file.absolute().resolve())
+
+    yield new_env
+
+    config_file.unlink()
 
 
 @pytest.fixture(autouse=True)
-def run_daemon():
+def run_daemon(request, env_with_config):
     """Run daemon every time tests on this file/module is run."""
+    if "integration" not in request.keywords:
+        yield
+        return
+
     pid_file = pathlib.Path(tempfile.gettempdir()) / "zippy_daemon.pid"
     assert not pid_file.exists()
 
-    Popen("./src/client/daemon.py", shell=True).wait()
+    Popen("./src/client/daemon.py", shell=True, env=env_with_config).wait()
 
     timeout = time.time() + 5  # 5 sec
 
@@ -32,6 +66,24 @@ def run_daemon():
 
     yield
 
+    # pid might have changed after restart, so read again
+    if pid_file.exists():
+        new_pid = int(pid_file.read_text().strip())
+    else:
+        # no harm on trying to kill twice. if there's no new pid,
+        # use old one as new again.
+        new_pid = pid
+
+    # cleanup
+    while True:
+        try:
+            os.kill(new_pid, signal.SIGTERM)
+            time.sleep(0.1)
+        except OSError:
+            if pid_file.exists():
+                pid_file.unlink()
+            break
+
     # cleanup
     while True:
         try:
@@ -44,7 +96,6 @@ def run_daemon():
 
 
 @pytest.mark.integration
-@pytest.mark.xfail
 def test_that_daemon_runs():
     """Test that daemon is running."""
 
@@ -60,7 +111,6 @@ def test_that_daemon_runs():
 
 
 @pytest.mark.integration
-@pytest.mark.xfail
 def test_that_daemon_cleans_up_when_killed():
     """Test that daemon cleans up when killed."""
     pid_file = pathlib.Path(tempfile.gettempdir()) / "zippy_daemon.pid"
@@ -80,13 +130,12 @@ def test_that_daemon_cleans_up_when_killed():
 
 
 @pytest.mark.integration
-@pytest.mark.xfail
-def test_that_daemon_stops_when_stop_is_called():
+def test_that_daemon_stops_when_stop_is_called(env_with_config):
     """Test that daemon stops up stop is called."""
     pid_file = pathlib.Path(tempfile.gettempdir()) / "zippy_daemon.pid"
     pid = int(pid_file.read_text().strip())
 
-    Popen("./src/client/daemon.py stop", shell=True).wait()
+    Popen("./src/client/daemon.py stop", shell=True, env=env_with_config).wait()
 
     assert not pid_file.exists()
 
@@ -95,13 +144,12 @@ def test_that_daemon_stops_when_stop_is_called():
 
 
 @pytest.mark.integration
-@pytest.mark.xfail
-def test_that_daemon_restarts_when_restart_is_called():
+def test_that_daemon_restarts_when_restart_is_called(env_with_config):
     """Test that daemon restarts if restart is called."""
     pid_file = pathlib.Path(tempfile.gettempdir()) / "zippy_daemon.pid"
     pid = int(pid_file.read_text().strip())
 
-    Popen("./src/client/daemon.py restart", shell=True).wait()
+    Popen("./src/client/daemon.py restart", shell=True, env=env_with_config).wait()
 
     # old process should have been stopped
     with pytest.raises(OSError):
@@ -120,14 +168,13 @@ def test_that_daemon_restarts_when_restart_is_called():
 
 
 @pytest.mark.integration
-@pytest.mark.xfail
-def test_if_daemon_running_again_start_should_fail():
+def test_if_daemon_running_again_start_should_fail(env_with_config):
     """Test that daemon does not start when another instance is already running."""
     pid_file = pathlib.Path(tempfile.gettempdir()) / "zippy_daemon.pid"
     pid = int(pid_file.read_text().strip())
 
     # should exit with 1 if already running
-    child = Popen("./src/client/daemon.py start", shell=True)
+    child = Popen("./src/client/daemon.py start", shell=True, env=env_with_config)
     child.communicate()
     assert child.returncode == 1
 
@@ -144,3 +191,58 @@ def test_if_daemon_running_again_start_should_fail():
     assert pid_file.exists()
     new_pid = int(pid_file.read_text().strip())
     assert new_pid
+
+
+@pytest.mark.unittest
+def test_daemon_start():
+    """Test that daemon works."""
+    with mock.patch.object(daemon.Daemon, "_daemonize") as daemonizer:
+        with mock.patch.object(daemon.sys, "exit") as mock_exit:
+            with mock.patch.object(daemon.Daemon, "_save_pid") as pid_save:
+                with mock.patch.object(daemon.Daemon, "_get_pid") as get_pid:
+                    get_pid.return_value = None
+                    daemon.Daemon().start()
+                get_pid.assert_called_once()
+            pid_save.assert_called_once()
+        mock_exit.assert_not_called()
+    daemonizer.assert_called_once()
+
+
+@pytest.mark.unittest
+def test_daemon_start_when_pid_already_exists():
+    """Test that the program exits if pid already exists."""
+    with mock.patch.object(daemon.Daemon, "_daemonize") as daemonizer:
+        with mock.patch.object(daemon.Daemon, "_save_pid") as pid_save:
+            with mock.patch.object(daemon.Daemon, "_get_pid") as get_pid:
+                get_pid.return_value = 2
+                with pytest.raises(SystemExit) as exc:
+                    daemon.Daemon().start()
+                assert exc.value.code == 1
+            get_pid.assert_called_once()
+        pid_save.assert_not_called()
+    daemonizer.assert_not_called()
+
+
+@pytest.mark.unittest
+def test_daemon_stop_when_doesnot_exist():
+    """Test daemon stop if daemon is not running already."""
+    with mock.patch.object(daemon.Daemon, "_get_pid") as get_pid:
+        with mock.patch.object(daemon.os, "kill") as nuke:
+            get_pid.return_value = None
+            assert daemon.Daemon().stop() is None
+    nuke.assert_not_called()
+    get_pid.assert_called_once()
+
+
+@pytest.mark.unittest
+def test_daemon_stop_when_pid_exists():
+    """Test daemon stop if daemon is already running."""
+    with mock.patch.object(daemon.Daemon, "_get_pid") as get_pid:
+        with mock.patch.object(daemon.Daemon, "_remove_pidfile") as pid_remover:
+            with mock.patch.object(daemon.os, "kill") as nuke:
+                get_pid.return_value = 3
+                nuke.side_effect = OSError
+                assert daemon.Daemon().stop() is None
+        pid_remover.assert_called_once()
+    nuke.assert_called_once_with(3, signal.SIGTERM)
+    get_pid.assert_called_once()
--- zippy/client/daemon.py
+++ zippy/client/daemon.py
@@ -11,18 +11,18 @@ import sys
 import tempfile
 import time
 
-import yaml
-
 from imapclient import IMAPClient
 from imapclient.exceptions import LoginError
 
 if __package__:
     # sphinx imports this module, so need to use relative imports for the purpose
     from .log import get_logger
+    from .config import get_config
 else:
     # running this as script doesnot work with relative imports as there is no parent
     # TODO: split these and put it somewhere else to remove this kind of checks
     from log import get_logger  # type: ignore  #pylint: disable=no-name-in-module
+    from config import get_config  # type: ignore  #pylint: disable=no-name-in-module
 
 
 LOGGER = get_logger()
@@ -90,7 +90,8 @@ class Daemon:
 
     def _remove_pidfile(self):
         LOGGER.info("Removing PID file...")
-        self.PID_FILE.unlink()
+        if self.PID_FILE.exists():
+            self.PID_FILE.unlink()
 
     def start(self):
         """Start daemon."""
@@ -128,6 +129,9 @@ class Daemon:
     def stop(self):
         """Kill the running daemon."""
         # Try killing the daemon process
+        if self.disable_daemon:
+            return
+
         pid = self._get_pid()
 
         if not pid:
@@ -143,13 +147,53 @@ class Daemon:
                 os.kill(pid, signal.SIGTERM)
                 time.sleep(0.2)
             except OSError:
-                if self.PID_FILE.exists():
-                    self.PID_FILE.unlink()
-                    LOGGER.info("Removed PID file")
+                self._remove_pidfile()
                 break
 
 
-def download_new_email(username, password, host, imap_port=993, timeout=3):
+def block_till_new_mail(server, timeout=60):
+    """Return true if there's new mail, else checks for timeout."""
+    failure = 0
+
+    while time.time() < timeout:
+        if failure >= 10:
+            failure = 1
+
+        try:
+            # Wait for up to 30 seconds for an IDLE response
+            responses = server.idle_check(timeout=30)
+            # if server was up, response should have come as timeout is 30sec.
+            # so, it's not required to check for socket.error here.
+        # log the unexpected exception
+        except socket.error:
+            failure += 1
+            LOGGER.info("Sleeping for %s second", failure * 10)
+            time.sleep(failure * 10)
+            continue
+        except Exception:  # pylint: disable=broad-except
+            LOGGER.exception("Unexpected error occurred.")
+            raise
+        else:
+            failure = 0
+            if responses:
+                return True
+        time.sleep(10)
+
+    try:
+        server.idle_done()
+    # nothing expected exception here. but let's log these if it occurs.
+    except Exception:  # pylint: disable=broad-except
+        LOGGER.exception("Unexpected error occurred.")
+        raise
+    else:
+        LOGGER.info("\nIDLE mode done")
+        time.sleep(10)
+        return False
+
+
+def download_new_email(
+    username, password, host, imap_port=993, timeout=3
+):  # pylint:disable=too-many-branches,too-many-statements
     """Check for new emails and download them.
 
     Parameters
@@ -179,74 +223,68 @@ def download_new_email(username, password, host, imap_port=993, timeout=3):
 
     LOGGER.debug(username)
 
-    try:
-        server = IMAPClient(
-            host, port=imap_port, ssl=True, ssl_context=ssl_context, timeout=timeout
-        )
-    except socket.error:
-        LOGGER.exception("Could not connect to the mail server.")
-        return
-    except ssl.SSLError:
-        LOGGER.exception("Error due to ssl.")
-        return
-
-    try:
-        server.login(username, password)
-        server.select_folder("INBOX")
-    except LoginError:
-        LOGGER.exception("Credentials could not be verified for '%s'.", username)
-        server.shutdown()
-        return
+    failure = 0
 
     while True:
-        # reconnect every 7 mins, as per recommendation to do so.
-        timeout = time.time() + 7 * 60  # after 7 mins
+        if failure > 10:
+            failure = 1
 
-        # Start IDLE mode
         try:
-            server.idle()
-        except Exception:  # pylint: disable=broad-except
-            LOGGER.exception("Unexpected exception occurred.")
-            return
-        else:
-            LOGGER.info(
-                "Connection is now in IDLE mode, send yourself an email or quit with ^c"
+            server = IMAPClient(
+                host, port=imap_port, ssl=True, ssl_context=ssl_context, timeout=timeout
             )
+        except socket.error:
+            LOGGER.exception("Could not connect to the mail server.")
+            failure += 1
+            LOGGER.info("Sleeping for %s second", failure * 10)
+            time.sleep(10 * failure)
+            continue
+        except ssl.SSLError:
+            LOGGER.exception("Error due to ssl.")
+            break
+        else:
+            failure = 0
 
-        curr_time = time.time()
-        while curr_time < timeout:
+        try:
+            server.login(username, password)
+            server.select_folder("INBOX")
+        except LoginError:
+            LOGGER.exception("Credentials could not be verified for '%s'.", username)
+            server.shutdown()
+            break
+        except socket.error:
+            LOGGER.exception("Could not connect to the mail server.")
+            failure += 1
+            time.sleep(10 * failure)
+            continue
+        else:
+            failure = 0
+
+        while True:
+            # reconnect every 7 mins, as per recommendation to do so.
+            timeout = int(time.time()) + 7 * 60  # after 7 mins
+
             try:
-                # Wait for up to 30 seconds for an IDLE response
-                responses = server.idle_check(timeout=30)
-                # if server was up, response should have come as timeout is 30sec.
-                # so, it's not required to check for socket.error here.
-            # log the unexpected exception
+                server.idle()
+            except socket.error:
+                LOGGER.exception("Could not connect to the mail server.")
+                break
             except Exception:  # pylint: disable=broad-except
-                LOGGER.exception("Unexpected error occurred.")
+                LOGGER.exception("Unexpected exception occurred.")
                 return
             else:
+                failure = 0
                 LOGGER.info(
-                    "Server sent: %s for %s",
-                    responses if responses else "nothing",
-                    username,
+                    "Connection is now in IDLE mode, send yourself an email or quit with ^c"
                 )
-            time.sleep(10)
 
-        try:
-            server.idle_done()
-        # nothing expected exception here. but let's log these if it occurs.
-        except Exception:  # pylint: disable=broad-except
-            LOGGER.exception("Unexpected error occurred.")
-            return
-        else:
-            LOGGER.info("\nIDLE mode done")
-            time.sleep(10)
+            has_new_mail = block_till_new_mail(server, timeout=timeout)
+            if has_new_mail:
+                pass
 
 
 if __name__ == "__main__":
-    STREAM = open(pathlib.Path(__file__).parents[2] / ".env.yml", "r")
-    CONFIG = yaml.safe_load(STREAM)
-    CLIENT_CONFIG = CONFIG.get("CLIENT")
+    CLIENT_CONFIG = get_config("CLIENT")
     if not CLIENT_CONFIG:
         print("Config not setup properly. Check env.example.yml for more info.")
         sys.exit(1)
