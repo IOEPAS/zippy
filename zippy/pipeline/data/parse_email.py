a79a429b7ef9d776064ad111f5c487eeec6644ce
divyamani <dma.htd@gmail.com>
2019-07-09 01:11:37 +0545

    Fix simple errors

--- zippy/pipeline/data/parse_email.py
+++ zippy/pipeline/data/parse_email.py
@@ -10,7 +10,7 @@ def get_text_from_email(msg):
     for part in msg.walk():
         if part.get_content_type() == "text/plain":
             parts.append(part.get_payload())
-    return "".join(parts)
+    return str.lower("".join(parts))
 
 
 def get_from_message(message):
@@ -21,7 +21,7 @@ def get_from_message(message):
     for key in keys:
         msg[key] = [str.lower(message[key])]
     msg["content"] = get_text_from_email(message)
-    msg["From"] = re.search(r"[\w_\-\.]+@[\w_\-\.]+\.[\w]+", msg["From"][0]).group(0)
+    msg["From"] = [re.search(r"[\w_\-\.]+@[\w_\-\.]+\.[\w]+", msg["From"][0]).group(0)]
     msg["is_thread"] = len(msg["Subject"][0].split("re: ")) > 1
     msg["Subject"] = msg["Subject"][0].strip("re: ")
     return msg
