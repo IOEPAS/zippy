e165e8b3fdcf490833918a09f439376edd579947
Saugat Pachhai <suagatchhetri@outlook.com>
2019-08-06 14:27:30 +0545

    Refactor json server

--- scripts/json_server.py
+++ scripts/json_server.py
@@ -4,13 +4,17 @@ import os
 import pathlib
 import socketserver
 
+from http import HTTPStatus
+from urllib.parse import urlparse
+
 from zippy.utils.json_generator import write_json_output
 
 PORT = 3000
 
 ROOT_DIR = (pathlib.Path(__file__).parent / "..").resolve()
 PUBLIC_DIR = ROOT_DIR / "public"
-LOG_FILE = ROOT_DIR / "output" / "logs" / "display" / "output.log"
+LOG_DIR = ROOT_DIR / "output" / "logs" / "display"
+LOG_FILE = LOG_DIR / "output.log"
 
 # Python3.7 has directory class attribute inside `SimpleHTTPRequestHandler`
 # but, to be compatible with 3.6, let's chdir into directory
@@ -28,21 +32,30 @@ class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
         super().end_headers()
 
 
-class RequestHandler(CORSRequestHandler):
-    """Request Handler that serves log file in json output as well as complete directory."""
+class JSONLogRequestHandler(http.server.SimpleHTTPRequestHandler):
+    """Return json log."""
 
-    def do_GET(self):
-        """Return GET response."""
-        if not self.path.startswith("/logs"):
-            super().do_GET()
-            return
-
-        self.send_response(200)
+    def do_json(self):
+        """Return json log with appropriate headers."""
+        self.send_response(HTTPStatus.OK)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
         with open(LOG_FILE, "rb") as file:
             write_json_output(file, self.wfile)
+
+
+class RequestHandler(JSONLogRequestHandler, CORSRequestHandler):
+    """Request Handler that serves log file in json output as well as complete directory."""
+
+    def do_GET(self):
+        """Return GET response."""
+        url = urlparse(self.path)
+        if url.path == "/logs":
+            self.do_json()
+            return
+
+        super().do_GET()
 
 
 # Python 3.7 has ThreadingHTTPServer that does this already
@@ -51,7 +64,9 @@ class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
 
 
 if __name__ == "__main__":
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+    LOG_FILE.touch()
+
     with ThreadingHTTPServer(("", PORT), RequestHandler) as httpd:
-        LOG_FILE.touch()
-        print("Started json server on port ", PORT)
+        print("Started json server on port", PORT)
         httpd.serve_forever()
